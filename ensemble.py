bb9099a501684e9d19cc831ed6b44e7d81b155c9
afunTW <endlessproof@gmail.com>
2018-07-26 12:13:12 +0800

    change paramter

--- ensemble.py
+++ ensemble.py
@@ -13,7 +13,7 @@ LOGGER = logging.getLogger(__name__)
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--method', dest='method', default='avg', choices=['avg', 'vote'])
-    parser.add_argument('--answer', dest='answer', default='data/test', required=True)
+    parser.add_argument('--ground-truth', dest='ground_truth', default='data/test', required=True)
     parser.add_argument('--predictions', dest='predictions', nargs='+')
     parser.add_argument('--predictions-dir', dest='predictions_dir', default='outputs')
     parser.add_argument('--predict-all', dest='predict_all', action='store_true')
@@ -42,7 +42,7 @@ def main(args):
         'avg': ensemble_avg,
         'vote': ensemble_vote
     }
-    X_test = list(Path(args.answer).glob('*/*'))
+    X_test = list(Path(args.ground_truth).glob('*/*'))
     y_true = [path.parent.name for path in X_test]
     y_true = np.array(list(map(int, y_true)))
     if args.predict_all:
